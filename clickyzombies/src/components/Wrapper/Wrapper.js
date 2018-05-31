d1ea39443d0fd6daf4e36a3a1f7440902302523c
Cdriver556 <cdriver556@gmail.com>
2018-05-30 22:00:27 -0400

    clean up

--- clickyzombies/src/components/Wrapper/Wrapper.js
+++ clickyzombies/src/components/Wrapper/Wrapper.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Wrapper.css";
 
-const Wrapper = props => <div className="wrapper">{props.children}<iframe width="1381" height="480" src="https://www.youtube.com/embed/9XaS93WMRQQ?autoplay=1" frameborder="0"  allow="autoplay; encrypted-media" allowfullscreen ></iframe></div>;
+const Wrapper = props => <div className="wrapper">{props.children}<iframe className="zombie" width="1381" height="480" src="https://www.youtube.com/embed/9XaS93WMRQQ?autoplay=1" frameborder="0"  allow="autoplay; encrypted-media" allowfullscreen ></iframe></div>;
 
 export default Wrapper;
